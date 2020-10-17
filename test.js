1136fff8fb94e275bc55c1635d36e120f536a1ef
kristina-arezina <arezina.kristina23@gmail.com>
2020-10-17 11:07:08 -0400

    fixed testing

--- test.js
+++ test.js
@@ -1,14 +1,14 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require("cors");
-const fs = require('fs');
-const app = express();
-const port = process.env.PORT || 6001;
+let express = require('express');
+let bodyParser = require('body-parser');
+let cors = require("cors");
+let fs = require('fs');
+let app = express();
+let port = process.env.PORT || 6001;
 
 // Tensorflow library
-// const tf = require('@tensorflow/tfjs');
-// const { response } = require('express');
-// const { model, train } = require('@tensorflow/tfjs');
+let tf = require('@tensorflow/tfjs');
+let { response } = require('express');
+let { model, train } = require('@tensorflow/tfjs');
 
 app.use(cors());
 app.use(bodyParser.json({limit: '50mb'}));
@@ -23,7 +23,7 @@ let has_resumes = false;
 let current_question = [];
 let new_ys = [];
 let model_loss = 10;        // Must he larger than the required_loss
-const required_loss = 0.1;  // How good the model has to be before it's considered
+let required_loss = 0.1;  // How good the model has to be before it's considered
 
 app.post('/api/world', cors(), (req, res) => {
     console.log("req.body", req.body);
@@ -31,8 +31,8 @@ app.post('/api/world', cors(), (req, res) => {
     let data = JSON.stringify(req.body);
   
     // If the script does not have any resumes, assume that the front end is sending those
-    if (has_resumes = false) {
-  
+    if (has_resumes == false) {
+        
       // Extract all the resumes into a list
       raw_resumes = [];
       for (let i in data) { raw_resumes.push(i); }
@@ -109,7 +109,7 @@ app.post('/api/world', cors(), (req, res) => {
         // All the resumes should now be preprocessed
       resumes = processedResumes;
       has_resumes = true;
-  
+      train2()
     } 
     else 
     {
@@ -173,96 +173,101 @@ app.post('/api/world', cors(), (req, res) => {
     
   });
 
+// Debug statement, app is running
+app.listen(6001, () => console.log(`Listening on port 6001`));
+
   // Tensorflow model
-// const input_dim     = 100;  // amount of words in the preprocessed resumes
-// const embedding_dim = 64;   // dimensionality of the embedding
+function train2 () {
+    let input_dim     = 100;  // amount of words in the preprocessed resumes
+    let embedding_dim = 64;   // dimensionality of the embedding
+    
+    let min_batch_size = 8;   // Minimum amount of data points required to train the model
+    let max_batch_size = 32;  // Max batch size used by model
+    
+    let compiled_model = createModel();
+    
+    while (model_loss > required_loss) {
+    
+      trainModel();
+    
+    }
+    
+    function createEncoder() {
+      let input = tf.input({shape: [input_dim]});
+    
+      let embedding = tf.layers.embedding({inputDim: input_dim, outputDim: embedding_dim})
+    
+      let flatten = tf.layers.flatten();
+    
+      let dense1 = tf.layers.dense({units: 2048, activation: "relu"});
+      let dense2 = tf.layers.dense({units: 1024, activation: "relu"});
+    
+      let output = dense2.apply( dense1.apply( flatten.apply( embedding.apply( input ) ) ) );
+    
+      return [input, output]
+    }
+    
+    function createModel() {
+      let encoder1 = createEncoder();
+      let encoder2 = createEncoder();
+    
+      let i1 = encoder1[0];
+      let o1 = encoder1[1];
+    
+      let i2 = encoder2[0];
+      let o2 = encoder2[1];
+    
+      let combiner = tf.layers.concatenate();
+    
+      let dense1 = tf.layers.dense({units: 1024, activation: "relu"});
+      let dense2 = tf.layers.dense({units: 256 , activation: "relu"});
+      let dense3 = tf.layers.dense({units: 64  , activation: "relu"});
+      let dense4 = tf.layers.dense({units: 2   , activation: "sigmoid"});
+    
+      let output = dense4.apply( dense3.apply( dense2.apply( dense1.apply(
+        combiner.apply([o1, o2])
+      ) ) ) );
+    
+      let precompiled_model = tf.model({inputs: [i1, i2], outputs: output});
+    
+      precompiled_model.compile({optimizer: "adam", loss: "categoricalCrossentropy"})
+    
+      return precompiled_model;
+    }
+    
+    function trainModel () {
+    
+      if (!has_resumes) { return; }
+    
+      // Look for new data points
+      if (new_xs_A.length != 0) {
+    
+        Xs_A = Xs_A.concat(new_xs_A);
+        Xs_B = Xs_B.concat(new_xs_B);
+        ys   = ys.concat(new_ys);
+    
+        new_xs_A = [];
+        new_xs_B = [];
+        new_ys   = [];
+    
+      }
+    
+      // See if there are enough data points to train the model
+      if (Xs_A.length >= min_batch_size) {
+    
+        // Set the batch size to the correct value
+        let batch_size = Math.min(max_batch_size, Xs_A.length);
+    
+        // Train the model
+        let history = compiled_model.fit([Xs_A, Xs_B], ys, {
+          epochs: 1, batchSize: batch_size
+        });
+    
+        model_loss = history.history.loss;
+      }
+    
+    }
+}
 
-// const min_batch_size = 8;   // Minimum amount of data points required to train the model
-// const max_batch_size = 32;  // Max batch size used by model
 
-// const compiled_model = createModel();
-
-// while (model_loss > required_loss) {
-
-//   trainModel();
-
-// }
-
-// function createEncoder() {
-//   const input = tf.input({shape: [input_dim]});
-
-//   const embedding = tf.layers.embedding({inputDim: input_dim, outputDim: embedding_dim})
-
-//   const flatten = tf.layers.flatten();
-
-//   const dense1 = tf.layers.dense({units: 2048, activation: "relu"});
-//   const dense2 = tf.layers.dense({units: 1024, activation: "relu"});
-
-//   const output = dense2.apply( dense1.apply( flatten.apply( embedding.apply( input ) ) ) );
-
-//   return [input, output]
-// }
-
-// function createModel() {
-//   let encoder1 = createEncoder();
-//   let encoder2 = createEncoder();
-
-//   let i1 = encoder1[0];
-//   let o1 = encoder1[1];
-
-//   let i2 = encoder2[0];
-//   let o2 = encoder2[1];
-
-//   const combiner = tf.layers.concatenate();
-
-//   const dense1 = tf.layers.dense({units: 1024, activation: "relu"});
-//   const dense2 = tf.layers.dense({units: 256 , activation: "relu"});
-//   const dense3 = tf.layers.dense({units: 64  , activation: "relu"});
-//   const dense4 = tf.layers.dense({units: 2   , activation: "sigmoid"});
-
-//   const output = dense4.apply( dense3.apply( dense2.apply( dense1.apply(
-//     combiner.apply([o1, o2])
-//   ) ) ) );
-
-//   const precompiled_model = tf.model({inputs: [i1, i2], outputs: output});
-
-//   precompiled_model.compile({optimizer: "adam", loss: "categoricalCrossentropy"})
-
-//   return precompiled_model;
-// }
-
-// function trainModel () {
-
-//   if (!has_resumes) { return; }
-
-//   // Look for new data points
-//   if (new_xs_A.length != 0) {
-
-//     Xs_A = Xs_A.concat(new_xs_A);
-//     Xs_B = Xs_B.concat(new_xs_B);
-//     ys   = ys.concat(new_ys);
-
-//     new_xs_A = [];
-//     new_xs_B = [];
-//     new_ys   = [];
-
-//   }
-
-//   // See if there are enough data points to train the model
-//   if (Xs_A.length >= min_batch_size) {
-
-//     // Set the batch size to the correct value
-//     const batch_size = Math.min(max_batch_size, Xs_A.length);
-
-//     // Train the model
-//     const history = compiled_model.fit([Xs_A, Xs_B], ys, {
-//       epochs: 1, batchSize: batch_size
-//     });
-
-//     model_loss = history.history.loss;
-//   }
-
-// }
-
-  // Debug statement, app is running
-  app.listen(6001, () => console.log(`Listening on port 6001`));
+  
